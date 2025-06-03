8800e1c2c1040e15b102b69046c7f72f979a1ba5
Sneh Yadav <snehyadav091@gmail.com>
2025-06-03 15:03:15 +0530

    day5 upload

---
+++ DAY-5/BooksApi.Entities/Entities/BookDetails.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BooksApi.Entities.Entities
+{
+    public class BookDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // auto-increment ID
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+    }
+}
